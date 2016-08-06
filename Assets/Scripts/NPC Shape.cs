21e5c0ce368b53db3a1e374184bdf509480af14f
Thaccus <Thaccus@gmail,com>
2016-08-06 09:38:53 -0700

    NPC

---
+++ Assets/Scripts/NPC Shape.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCShape : MonoBehaviour {
+
+    public
+	// Use this for initialization
+	void Start () {
+       Texture2D  TextureA[] = new Texture2D[20];
+        for(int i=0; i<20; i++)
+            TextureA[i]= TextureA.
+	}
+
+
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        shape = (int)Random.Range(0.0f, 19.99f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Collision2D.Equals( PlayerCtrl, NPCShape)
+        {
+            
+        }
+	}
+
+    bool shapeMatch(int pShape)
+    {
+        return (pShape == shape);
+    }
+}
--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -7,9 +7,13 @@ public class PlayerCtrl : MonoBehaviour {
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Update is called once per frame
+     public float speed = 2.0f;
+ 
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
 }
