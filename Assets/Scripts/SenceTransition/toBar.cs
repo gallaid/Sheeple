54feba03226c560ce8f95ac3b056ccdf973cbd65
gallaid <spencer@spencersmoot.com>
2016-08-06 16:07:00 -0700

    ScenecTrasiton moves

---
+++ Assets/Scripts/SenceTransition/toBar.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RoomSwitch3 : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D thing)
+    {
+        if (thing.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Bar");
+        }
+
+    }
+
+}
+
---
+++ Assets/Scripts/SenceTransition/toStadium.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RoomSwitch4 : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D thing)
+    {
+        if (thing.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Stadium");
+        }
+
+    }
+
+}
+
