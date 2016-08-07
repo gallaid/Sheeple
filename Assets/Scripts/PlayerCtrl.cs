f3f67391fc5f3fae793e20c9c54bb2bf4abe7ed0
Thaccus <Thaccus@gmail,com>
2016-08-06 21:06:23 -0700

    SAnimation Test

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 
 public class PlayerCtrl : MonoBehaviour {
-
+    //Animator anim;
 	// Use this for initialization
 	void Start () {
-	
+    //    anim = GetComponent<Animator>();
 	}
 
     // Update is called once per frame
@@ -15,5 +15,11 @@ public class PlayerCtrl : MonoBehaviour {
     {
         var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         transform.position += move * speed * Time.deltaTime;
+       // int moveHash = Animator.StringToHash("IsWalking");
+
+       // if (Input.anyKeyDown)
+       //     anim.SetTrigger(moveHash);
+
     }
+
 }
