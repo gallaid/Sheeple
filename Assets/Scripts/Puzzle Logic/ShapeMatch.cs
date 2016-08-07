47a1a3003ab4734ad5c08facf057726b1ba1ccb8
Thaccus <Thaccus@gmail,com>
2016-08-07 11:24:09 -0700

    Shape Match framework
    Setting up skeleton for minigame

---
+++ Assets/Scripts/Puzzle Logic/ShapeMatch.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeMatch : MonoBehaviour {
+
+    enum MiniState {NPCDISPLAY, PLAYERGUESS, RESETPUZZLE, WIN, LOSE};
+    MiniState miniState;
+
+
+
+	// Use this for initialization
+	void Start () {
+        miniState = MiniState.NPCDISPLAY;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch(miniState)
+        {
+            case MiniState.NPCDISPLAY:
+                break;
+            case MiniState.PLAYERGUESS:
+                break;
+            case MiniState.RESETPUZZLE:
+                break;
+            case MiniState.WIN:
+                break;
+            case MiniState.LOSE:
+                break;
+            default:
+                break;
+        }
+	
+	}
+}
