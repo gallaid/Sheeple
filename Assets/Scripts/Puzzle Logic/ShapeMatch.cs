bbdc48a06ec8dc824ae192a3b4c340db852de911
Thaccus <Thaccus@gmail,com>
2016-08-07 12:33:22 -0700

    Shape Match gamestate template

--- Assets/Scripts/Puzzle Logic/ShapeMatch.cs
+++ Assets/Scripts/Puzzle Logic/ShapeMatch.cs
@@ -5,6 +5,8 @@ public class ShapeMatch : MonoBehaviour {
 
     enum MiniState {NPCDISPLAY, PLAYERGUESS, RESETPUZZLE, WIN, LOSE};
     MiniState miniState;
+    enum DisplayState {SHOW1, SHOW2, SHOW3, END};
+    DisplayState displayState;
 
 
 
@@ -19,21 +21,62 @@ public class ShapeMatch : MonoBehaviour {
         switch(miniState)
         {
             case MiniState.NPCDISPLAY:
-                //Display
+                switch(displayState)
+                {
+                case DisplayState.SHOW1:
+                    break;
+                case DisplayState.SHOW2:
+                    break;
+                case DisplayState.SHOW3:
+                    break;
+                case DisplayState.END:
+                    displayState = DisplayState.SHOW1;
+                    miniState = MiniState.PLAYERGUESS;
+                    break;
+                default:
+                    print("Das Error(NPCDISPLAY.displayState");
+                    break;
+                }
                 break;
             case MiniState.PLAYERGUESS:
+                switch (displayState)
+                {
+                    case DisplayState.SHOW1:
+                        //if time < switch time 
+                        //show first color for time
+                        //else displayState = Displaystate.SHOW2
+                        break;
+                    case DisplayState.SHOW2:
+                        break;
+                    case DisplayState.SHOW3:
+                        break;
+                    case DisplayState.END:
+                        displayState = DisplayState.SHOW1;
+                        miniState = MiniState.PLAYERGUESS;
+                        break;
+                    default:
+                        print("Das Error(PLAYERGUESS.displayState)");
+                        break;
+                }
                 break;
             case MiniState.RESETPUZZLE:
+                //if wincounter != enough
+                {
+                    //Update win counter
+                    //randomize and clear puzzle arrays.
+                    miniState = MiniState.PLAYERGUESS;
+                }
+                //else win
                 break;
             case MiniState.WIN:
                 break;
             case MiniState.LOSE:
                 break;
             default:
+                print("Das Error(miniState)");
                 break;
         }
 	
 	}
 
-    void 
 }
