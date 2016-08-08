ead6f376459d90ef0aeaf6a77d7f91c0107aba95
Thaccus <Thaccus@gmail,com>
2016-08-07 17:39:30 -0700

    Added Prefabs and More scene work

---
+++ Assets/Scripts/NPCTrigger3D.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCTrigger3D : MonoBehaviour {
+
+    // Update is called once per frame
+
+
+    void OnTriggerEnter(Collider thing)
+    {
+        if (thing.gameObject.tag == "Player")
+        {
+
+        }
+    }
+
+
+}
